7a11859cb275f12098cf7e8b6b6c4b46bc623400
BrunoF2P <brunobispof2p@gmail.com>
2024-11-02 00:54:46 -0300

    feat: implementação do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import pipocaImg from "./src/assets/pipoca.png";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
