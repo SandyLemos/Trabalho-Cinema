db3ba4dbdac893c7543402a8627c7b8322dbd420
BrunoF2P <brunobispof2p@gmail.com>
2024-11-02 19:40:18 -0300

    feat: Implementando obter estados

---
+++ src/repositories/EstadosRepository.js
@@ -0,0 +1,31 @@
+import api from '../utils/api';
+import EstadoModel from "../models/EstadoModel.js";
+
+class EstadosRepository {
+    async getAllStates() {
+        try {
+            const response = await api.get('/states');
+            // Verificamos se a resposta está no formato esperado
+            if (response.data.success && response.data.states) {
+                return response.data.states.map(EstadoModel.fromApi);
+            } else {
+                throw new Error("Formato de resposta inesperado");
+            }
+        } catch (error) {
+            // Lida com possíveis erros da requisição
+            if (error.response && error.response.data) {
+                const errorData = error.response.data;
+
+                if (Array.isArray(errorData.erro)) {
+                    const primeiroErro = errorData.erro[0].msg;
+                    throw new Error(primeiroErro);
+                } else if (errorData.msg) {
+                    throw new Error(errorData.msg);
+                }
+                throw new Error("Erro desconhecido");
+            }
+        }
+    }
+}
+
+export default new EstadosRepository;
