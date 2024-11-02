0005ebe81380fbcf64e7a8addc0e9d3905cff32f
BrunoF2P <brunobispof2p@gmail.com>
2024-11-02 19:41:31 -0300

    feat: Implementando cadastrar usuario

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -9,6 +9,27 @@ class AuthController {
             throw new Error(error.message);
         }
     }
+
+    async register(nome, email, senha, re_senha, cpf, data_nascimento, id_estado, id_cidade, codigo_ref = "") {
+        try {
+            const registeredUserData = await AuthRepository.registrar(
+                nome,
+                email,
+                senha,
+                re_senha,
+                cpf.replace(/\D/g, ''),
+                data_nascimento,
+                parseInt(id_estado, 10),
+                parseInt(id_cidade, 10),
+                codigo_ref
+            );
+            return registeredUserData; // Retorna os dados do usuário registrado
+        } catch (error) {
+            throw new Error(error.message); // Retorna o erro se ocorrer
+        }
+    }
 }
+
+
 
 export default new AuthController();
