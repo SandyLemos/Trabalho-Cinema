4056380484ae59ee11f95d3e1339bf9946803b1b
BrunoF2P <brunobispof2p@gmail.com>
2024-11-02 19:30:12 -0300

    feat: adicionando pagina de não encontrado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Entrar from './pages/Entrar.jsx';
 import Registrar from './pages/Registrar.jsx';
+import NotFound from "./pages/NotFound.jsx";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <Routes>
                 <Route path="/entrar" element={<Entrar />} />
                 <Route path="/registrar" element={<Registrar />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="bg-[#121212] text-white h-screen flex flex-col justify-center items-center text-center p-4">
+            <h1 className="text-5xl mb-4">404 - Página Não Encontrada</h1>
+            <p className="text-2xl">A página que você está procurando não existe.</p>
+        </div>
+    );
+};
+
+export default NotFound;
