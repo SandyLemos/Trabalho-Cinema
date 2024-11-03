82ca6789550b8954c932b3b40ace7ec1fead910d
BrunoF2P <brunobispof2p@gmail.com>
2024-11-02 22:48:42 -0300

    style: redesign da tela de registrar

--- src/pages/Registrar.jsx
+++ src/pages/Registrar.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 import cinemaLogo from '../assets/cinema_logo.png';
-import tapeImage from "../assets/tape.png";
 import iconeCadastro from '../assets/cadastro.png';
 import AuthController from "../controllers/AuthController.js";
 import EstadosController from "../controllers/EstadosController.js";
 import CidadesController from "../controllers/CidadesController.js";
 import {formatarCpf} from "../utils/formatarCpf.js";
 import {formatarTelefone} from "../utils/formatarTelefone.js";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 import '../styles/entrar.css'
 
 const Registrar = () => {
@@ -23,6 +24,8 @@ const Registrar = () => {
     const [cidade, setCidade] = useState('');
     const [senha, setSenha] = useState('');
     const [confirmarSenha, setConfirmarSenha] = useState('');
+    const [mostrarSenha, setMostrarSenha] = useState(false);
+    const [mostrarConfirmarSenha, setMostrarConfirmarSenha] = useState(false);
     const [concordaTermos, setConcordaTermos] = useState(false);
     const [mensagemErro, setMensagemErro] = useState('');
     const [isLoading, setIsLoading] = useState(false); // Estado de loading
@@ -32,6 +35,12 @@ const Registrar = () => {
     const [loadingEstados, setLoadingEstados] = useState(true);
     const [loadingCidades, setLoadingCidades] = useState(false);
 
+    const isVisiblePassword = () => {
+        setMostrarSenha(!mostrarSenha);
+    };
+        const isVisibleRePassword = () => {
+        setMostrarConfirmarSenha(!mostrarConfirmarSenha);
+    };
 
     const handleCpfChange = (event) => {
         const value = event.target.value;
@@ -132,53 +141,51 @@ const Registrar = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-color-grad relative">
-            <div className="bg-white rounded-full w-72 h-72 mb-16 mt-6 items-center justify-center flex shadow-lg z-10">
-                <img src={cinemaLogo} alt="Logo" className="w-52"/>
+            <div className="bg-white rounded-full w-96 h-96 mb-16 mt-6 items-center justify-center flex shadow-lg z-10">
+                <img src={cinemaLogo} alt="Logo" className="w-72"/>
             </div>
-            <img src={tapeImage} className="absolute left-0 top-[9%] w-1/4" alt="Enfeite Esquerdo"/>
-            <img src={tapeImage} className="absolute right-0 top-[9%] w-1/4" alt="Enfeite Direito"/>
-
-            <div className="bg-white shadow-lg rounded-lg p-8 mb-4 w-full max-w-screen-md relative z-10">
+            <div className="bg-white shadow-lg rounded-lg p-8 mb-4 w-full max-w-screen-lg relative z-10">
                 <div id="cabecalho" className="flex items-center mb-6">
-                    <img src={iconeCadastro} alt="Ícone de Registrar" className="w-10 mr-2"/>
-                    <h1 className="text-2xl font-bold">Registrar</h1>
+                    <img className="w-10 h-10 mr-2" src={iconeCadastro} alt="Ícone de Registrar" />
+                    <h1 className="text-6xl font-bold">Registrar</h1>
                 </div>
                 <form id="area_cadastro" onSubmit={handleRegister}>
-                    <h2 className="text-lg font-semibold mb-2">Sobre Você</h2>
-                    <input
+                    <h2 className="text-2xl font-bold mb-2 pt-8 pb-2">SOBRE VOCÊ</h2>
+                    <input className="border-[1px] border-black shadow-md h-14 focus:outline-none focus- focus:shadow-lg focus:shadow-gray-300/50 transition-shadow duration-200 rounded-lg p-2 w-full  mb-4"
                         type="text"
                         maxLength='14'
                         value={nome}
                         onChange={(e) => setNome(e.target.value)}
                         placeholder="Nome Completo"
-                        className="border border-gray-300 rounded-lg p-2 w-full mb-4"
+                        
                         required
                     />
-                    <div className="flex justify-between mb-4">
+                    <div className="flex justify-between mb-4 gap-12">
                         <input
                             type="text"
                             value={cpf}
                             onChange={handleCpfChange}
                             placeholder="CPF"
-                            className="border border-gray-300 rounded-lg p-2 w-1/2 mr-2"
+                            className="border-[1px] border-black shadow-md h-14 focus:outline-none focus- focus:shadow-lg focus:shadow-gray-300/50 transition-shadow duration-200 rounded-lg p-2 w-1/2 mr-2"
                             required
                         />
                         <input
                             type="date"
                             value={dataNascimento}
                             onChange={(e) => setDataNascimento(e.target.value)}
-                            className="border border-gray-300 rounded-lg p-2 w-1/2 ml-2"
+                            className="border-[1px] border-black shadow-md h-14 focus:outline-none focus- focus:shadow-lg focus:shadow-gray-300/50 transition-shadow duration-200 rounded-lg p-2 w-1/2 ml-2"
                             required
                         />
+                        
                     </div>
-                    <h2 className="text-lg font-semibold mb-2">Informações de Contato</h2>
-                    <div className="flex justify-between mb-4">
+                    <h2 className="text-2xl font-bold mb-2 pt-4 pb-2">INFROAMÇÕES DE CONTATO</h2>
+                    <div className="flex justify-between mb-4 gap-12">
                         <input
                             type="email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             placeholder="Email"
-                            className="border border-gray-300 rounded-lg p-2 w-1/2 mr-2"
+                            className="border-[1px] border-black shadow-md h-14 focus:outline-none focus- focus:shadow-lg focus:shadow-gray-300/50 transition-shadow duration-200 rounded-lg p-2 w-1/2 mr-2"
                             required
                         />
                         <input
@@ -186,16 +193,16 @@ const Registrar = () => {
                             value={telefone}
                             onChange={handleTelefoneChange}
                             placeholder="Telefone"
-                            className="border border-gray-300 rounded-lg p-2 w-1/2 ml-2"
+                            className="border-[1px] border-black shadow-md h-14 focus:outline-none focus- focus:shadow-lg focus:shadow-gray-300/50 transition-shadow duration-200 rounded-lg p-2 w-1/2 ml-2"
                             required
                         />
                     </div>
-                    <h2 className="text-lg font-semibold mb-2">Endereço</h2>
-                    <div className="flex justify-between mb-4">
+                    <h2 className="text-2xl font-bold mb-2 pt-4 pb-2">ENDEREÇO</h2>
+                    <div className="flex justify-between mb-4 gap-12">
                         <select
                             value={estado}
                             onChange={handleEstadoChange}
-                            className="border border-gray-300 rounded-lg p-2 w-1/2 mr-2"
+                            className="border-[1px] border-black shadow-md h-14 focus:outline-none focus- focus:shadow-lg focus:shadow-gray-300/50 transition-shadow duration-200 rounded-lg p-2 w-1/2 mr-2"
                             required
                         >
                             {estado === "" && <option value="">Selecione o Estado</option>}
@@ -214,7 +221,7 @@ const Registrar = () => {
                         <select
                             value={cidade}
                             onChange={(e) => setCidade(e.target.value)}
-                            className="border border-gray-300 rounded-lg p-2 w-1/2 ml-2"
+                            className="border-[1px] border-black shadow-md h-14 focus:outline-none focus- focus:shadow-lg focus:shadow-gray-300/50 transition-shadow duration-200 rounded-lg p-2 w-1/2 ml-2"
                             required
                             disabled={!estado}
                         >
@@ -235,26 +242,43 @@ const Registrar = () => {
                             )}
                         </select>
                     </div>
-                    <h2 className="text-lg font-semibold mb-2">Crie sua Senha</h2>
-                    <div className="flex justify-between mb-4">
-                        <input
-                            type="password"
-                            value={senha}
-                            onChange={(e) => setSenha(e.target.value)}
-                            placeholder="Senha"
-                            className="border border-gray-300 rounded-lg p-2 w-1/2 mr-2"
-                            required
-                        />
-                        <input
-                            type="password"
-                            value={confirmarSenha}
-                            onChange={(e) => setConfirmarSenha(e.target.value)}
-                            placeholder="Confirme sua Senha"
-                            className="border border-gray-300 rounded-lg p-2 w-1/2 ml-2"
-                            required
-                        />
+                    <h2 className="text-2xl font-bold mb-2 pt-4 pb-2">CRIE SUA SENHA</h2>
+                    <div className="flex justify-between mb-4 gap-12">
+                        <div className="relative w-1/2 mr-2">
+                            <input
+                                type={mostrarSenha ? "text" : "password"}
+                                value={senha}
+                                onChange={(e) => setSenha(e.target.value)}
+                                placeholder="Senha"
+                                className="border-[1px] border-black shadow-md h-14 focus:outline-none focus:shadow-lg focus:shadow-gray-300/50 transition-shadow duration-200 rounded-lg p-2 w-full pr-10"
+                                required
+                            />
+                            <div
+                                className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                                onClick={isVisiblePassword}
+                            >
+                                {mostrarSenha ? <FaEyeSlash /> : <FaEye />}
+                            </div>
+                        </div>
+
+                        <div className="relative w-1/2 ml-2">
+                            <input
+                                type={mostrarConfirmarSenha ? "text" : "password"}
+                                value={confirmarSenha}
+                                onChange={(e) => setConfirmarSenha(e.target.value)}
+                                placeholder="Confirme sua Senha"
+                                className="border-[1px] border-black shadow-md h-14 focus:outline-none focus:shadow-lg focus:shadow-gray-300/50 transition-shadow duration-200 rounded-lg p-2 w-full pr-10" // Ajusta o padding à direita para o ícone
+                                required
+                            />
+                            <div
+                                className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                                onClick={isVisibleRePassword}
+                            >
+                                {mostrarConfirmarSenha ? <FaEyeSlash /> : <FaEye />}
+                            </div>
+                        </div>
                     </div>
-                    <span className="block mb-2">A senha deve conter entre 8 a 16 caracteres:</span>
+                    <span className="block mb-2 pt-4 pb-4" >A senha deve conter entre 8 a 16 caracteres:</span>
 
                     <div className="flex items-center mb-4">
                         <input
@@ -264,15 +288,15 @@ const Registrar = () => {
                             className="mr-2"
                             required
                         />
-                        <span>Li e Concordo com os termos de uso da empresa.</span>
+                        <span>Li e aceitos os termos de condição da empresa.</span>
                     </div>
                     {mensagemErro && (
                         <div className="text-red-500 mb-2">{mensagemErro}</div>
                     )}
-                    <div className="flex justify-center mt-4">
+                    <div className="flex justify-center mt-4 pt-2">
                         <button
                             type="submit"
-                            className="bg-purple-500 text-white rounded-lg px-4 py-2 transition hover:bg-purple-600"
+                            className="bg-purple-500 text-white rounded-lg px-4 py-2 h-12 transition hover:bg-purple-600"
                             disabled={isLoading}
                         >
                             {isLoading && (
