e76aba858cc155d378d69ef95d9bce3c1624a562
BrunoF2P <brunobispof2p@gmail.com>
2024-11-05 01:07:32 -0300

    feat: impelementando chamadas de generos

---
+++ src/services/GenerosService.js
@@ -0,0 +1,40 @@
+import GenerosRepository from "../repositories/GenerosRepository.js";
+
+class GenerosService{
+    async createGenre(nome_genero) {
+        try {
+            const genre = {
+                nome_genero,
+            };
+            return await GenerosRepository.createGenre(genre);
+        } catch (error) {
+            throw new Error(error.message || "Erro na criação do genero");
+        }
+    }
+
+
+    async updateGenre(id, nome_genero) {
+        try {
+            return await GenerosRepository.updateGenre(id, nome_genero );
+        } catch (error) {
+            throw new Error(error.message || "Erro na atualização do gênero");
+        }
+    }
+    async getGenreById(id) {
+        try {
+            return await GenerosRepository.getGenreById(id);
+        } catch (error) {
+            throw new Error(error.message || "Erro ao buscar o gênero");
+        }
+    }
+
+    async deleteGenre(id) {
+        try {
+            return await GenerosRepository.deleteGenre(id);
+        } catch (error) {
+            throw new Error(error.message || "Erro ao deletar o gênero");
+        }
+    }
+}
+
+export default new GenerosService();
