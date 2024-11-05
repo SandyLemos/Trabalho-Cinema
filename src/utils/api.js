8e57f13175ef460a3bd3ad4a0f3215d0f0675d81
BrunoF2P <brunobispof2p@gmail.com>
2024-11-05 01:02:36 -0300

    feat: todas as requisicoes usará cookie de validação

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // Cria uma conexão com a API
 const api = axios.create({
     baseURL: 'https://roasted-haley-cinema-tech-02703ea5.koyeb.app/v1', // URL base da API
+    withCredentials: true,
     timeout: 10000, // Tempo limite da requisição
 });
 
